d8b1017add99ca151ae8f9e2271de8860f593845
Admin <Admin@DESKTOP-BC4UCVT>
2022-10-08 12:20:11 +0530

    first commite to ganesh

---
+++ aniket/src/aniket/AniketK.java
@@ -0,0 +1,16 @@
+package aniket;
+
+class ABC {
+ 
+}
+
+public class AniketK {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("hello world");
+	
+
+	}
+
+}
