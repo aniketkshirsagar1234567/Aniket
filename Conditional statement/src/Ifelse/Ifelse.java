c7eb8b282bf60ad77a7c3eabc82a738f6efe23d7
Admin <Admin@DESKTOP-SL5KEUD>
2022-10-08 11:29:12 +0530

    First commit in akash

---
+++ Conditional statement/src/Ifelse/Ifelse.java
@@ -0,0 +1,22 @@
+package Ifelse;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+	
+		int a = 95;
+		int b = 55;
+		System.out.println("LETS START");
+		if (a>b)
+		{
+			System.out.println("a is larger");
+		}
+		else 
+		{
+			System.out.println("b is larger");
+		}
+		}
+
+	
+
+}
---
+++ Conditional statement/src/Ifelse/Ifelse2.java
@@ -0,0 +1,21 @@
+package Ifelse;
+
+public class Ifelse2 {
+
+	public static void main(String[] args) {
+		int a = 101;
+		int b = 89;
+		System.out.println("Hurray");
+		if (a<=b)
+		{
+		System.out.println("a is big");
+		}
+		else
+		{
+			System.out.println("b is big");	
+			
+		}
+
+	}
+
+}
